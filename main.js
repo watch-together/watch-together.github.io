3192f4d0c946e6be2528ae0541fb771b1fdff130
Le Quang Huy <mtxno1@gmail.com>
2017-09-04 10:32:48 +0700

    data attribute

--- main.js
+++ main.js
@@ -29,10 +29,10 @@ function restrucSearchResult(array) {
 }
 
 function loadYT(id) {
-  if($("#youtube-container").data("id") === id) {
+  if ($("#youtube-container").data("id") === id) {
     return;
   }
-  $("#youtube-container").data("id") = id;
+  $("#youtube-container").data("id", id);
   $("#youtube-container").html(
     '<iframe width="480" height="270" src="https://www.youtube.com/embed/' +
       id +
